a763339d0eb7df6c191a5e10a240f3c0b1a790c5
akp <parida.akshaya@gmail.com>
2018-05-15 19:58:22 -0400

    Added

---
+++ spring-jdbctemplate/src/main/java/com/ap/springjdbctemplate/SpringJdbctemplateApplication.java
@@ -0,0 +1,12 @@
+package com.ap.springjdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbctemplateApplication.class, args);
+	}
+}
---
+++ spring-jdbctemplate/src/test/java/com/ap/springjdbctemplate/SpringJdbctemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ap.springjdbctemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJdbctemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
