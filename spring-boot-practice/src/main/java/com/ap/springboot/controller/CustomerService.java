4f9eb10f96101df9dd24a9e438e51f9a4daa0fd3
akp <parida.akshaya@gmail.com>
2018-05-15 20:32:12 -0400

    Updated

--- spring-boot-practice/src/main/java/com/ap/springboot/controller/CustomerService.java
+++ spring-boot-practice/src/main/java/com/ap/springboot/controller/CustomerService.java
@@ -4,19 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ap.springboot.bean.Customer;
 
-@RestController
+@RestController("/")
 public class CustomerService {
 
-	@GetMapping(value = "/customers")
-	public List<Customer> getCustomers(@RequestParam String param) {
+	@GetMapping(value = "customers")
+	public List<Customer> getCustomers() {
 		return getCustomersMock();
 	}
 	
+	@GetMapping(value = "customers/{customerId}")
+	public Customer getCustomer(@PathVariable("customerId") String customerId) {
+		return getCustomerMock();
+	}
 	
 	private List<Customer> getCustomersMock(){
 		List<Customer> customers = new ArrayList<>();
@@ -27,6 +32,12 @@ public class CustomerService {
 		customers.add(new Customer("Ashok4", "address1", "address2", "Iselin", "NJ", "08810", "2312312312", "abc@xyz.com", 5000));
 		customers.add(new Customer("Ashok5", "address1", "address2", "Iselin", "NJ", "08810", "2312312312", "abc@xyz.com", 5000));
 		return customers;
+	}
+	
+	private Customer getCustomerMock(){
+		
+		return new Customer("Ashok", "address1", "address2", "Iselin", "NJ", "08810", "2312312312", "abc@xyz.com", 5000);
+
 	}
 
 
